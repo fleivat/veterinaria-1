0bc822d63d30b2a5d849eb2b13b0cfab314a2a8f
fabian032 <85408017+fabian032@users.noreply.github.com>
2024-10-03 09:43:36 -0500

    Add files via upload
    ultima versión de la página realizada

---
+++ veterinaria-main/veterinaria/js/scripts.js
@@ -0,0 +1,44 @@
+// Mostrar y ocultar popup de contacto
+document.getElementById('contactBtn').addEventListener('click', function() {
+  document.getElementById('contactPopup').style.display = 'block';
+});
+
+document.getElementById('closePopupBtn').addEventListener('click', function() {
+  document.getElementById('contactPopup').style.display = 'none';
+});
+
+
+// Mostrar el popup de contacto
+document.getElementById("contactBtn").addEventListener("click", function() {
+  document.getElementById("contactPopup").style.display = "block";
+});
+// Cerrar el popup de contacto
+document.getElementById("closePopupBtn").addEventListener("click", function() {
+  document.getElementById("contactPopup").style.display = "none";
+});
+
+$('#loginBtn').click(function() {
+  $('#loginModal').modal('show');
+});
+
+ // Array de colores para la navbar
+ const colors = ['#64cce3', '#D291E3', '#7AD0E4', '#E3A064;', '#b7e36f','#B7E36F;'];
+ let currentColorIndex = 0;
+
+ 
+
+function changeNavbarColor() {
+  document.getElementById('navbar').style.opacity = 0;
+  setTimeout(() => {
+    document.getElementById('navbar').style.backgroundColor = colors[currentColorIndex];
+    document.getElementById('navbar').style.opacity = 1;
+  }, 500);
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+}
+
+ // Cambiar el color cada 3 segundos
+ setInterval(changeNavbarColor, 3000);
+
+ // Cambiar al primer color inmediatamente
+ changeNavbarColor();
+
